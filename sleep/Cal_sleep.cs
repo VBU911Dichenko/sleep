f591b910ceb8fe5f5ea33387bcf027d6bb1465ed
student <student@COMP-209>
2021-02-21 12:53:41 +0200

    Добоваил переменные для времени когда вствавать и ложиться и добовил переменные для  введенного времени сна колек5цию для хранения времени подёма

---
+++ sleep/Cal_sleep.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sleep
+{
+    class Cal_sleep
+    {
+        public List<DateTime> rezult { get; set; }
+        public double time_sleep { get; set; }
+        DateTime now { get; set; }
+        DateTime up { get; set; }
+        double interval;
+        public Cal_sleep()
+        {
+            rezult = new List<DateTime>();
+            interval = 1.5;
+        }
+        public void Set_Date() => now = DateTime.Now;
+        public List<DateTime> Calculating()
+        {
+            
+            Set_Date();
+            up = now.AddDays(time_sleep);
+
+            return rezult;
+        }
+
+    }
+}
